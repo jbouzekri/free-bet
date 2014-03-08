5dc4a34977bd2846ccde2c81126f2100aab933bf
jbouzekri <jonathan.bouzekri@gmail.com>
2014-03-08 20:50:47 +0100

    load gamble type configuration

---
+++ src/BettingSas/Bundle/GambleBundle/DependencyInjection/BettingSasGambleExtension.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace BettingSas\Bundle\GambleBundle\DependencyInjection;
+
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\Yaml\Yaml;
+use Symfony\Component\Config\FileLocator;
+
+/**
+ * BettingSasCompetitionBundleExtension.
+ *
+ * @author jobou
+ */
+class BettingSasGambleExtension extends Extension
+{
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $gambleConfig = array();
+
+        foreach ($container->getParameter('kernel.bundles') as $bundle) {
+            $reflection = new \ReflectionClass($bundle);
+            if (is_file($file = dirname($reflection->getFilename()) . '/Resources/config/gamble_types.yml')) {
+                $bundleConfig = Yaml::parse(realpath($file));
+
+                if (is_array($gambleConfig)) {
+                    $gambleConfig = array_merge($gambleConfig, $bundleConfig);
+                }
+            }
+        }
+
+        $container->setParameter('betting_sas.gamble.configuration', $gambleConfig);
+        //$loader = new YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        //$loader->load('services.yml');
+    }
+}
---
+++ src/BettingSas/Bundle/SoccerBundle/Gamble/WinnerGamble.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace BettingSas\Bundle\SoccerBundle\Gamble;
+
+/**
+ * Description of WinnerGamble
+ *
+ * @author jobou
+ */
+class WinnerGamble
+{
+    //put your code here
+}
