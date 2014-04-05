62b4300ac8e185f5440f4d58f2d46801284cbb5f
jbouzekri <jonathan.bouzekri@gmail.com>
2014-04-05 13:47:14 +0200

    Init UI bundle

---
+++ src/BettingSas/Bundle/UIBundle/BettingSasUIBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BettingSas\Bundle\CompetitionBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * BettingSas Competition Bundle
+ *
+ * @author jobou
+ */
+class BettingSasCompetitionBundle extends Bundle
+{
+}
