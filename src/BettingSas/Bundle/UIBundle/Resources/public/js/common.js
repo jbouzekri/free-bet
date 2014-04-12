904e759c7eda9ff80af711229b301c3ce5e13f52
jbouzekri <jonathan.bouzekri@gmail.com>
2014-04-12 21:40:43 +0200

    left col stretch height

---
+++ src/BettingSas/Bundle/UIBundle/Resources/public/js/common.js
@@ -0,0 +1,55 @@
+$(function() {
+    "use strict";
+
+    //Activate tooltips
+    $("[data-toggle='tooltip']").tooltip();
+
+    /*
+     * INITIALIZE BUTTON TOGGLE
+     * ------------------------
+     */
+    $('.btn-group[data-toggle="btn-toggle"]').each(function() {
+        var group = $(this);
+        $(this).find(".btn").click(function(e) {
+            group.find(".btn.active").removeClass("active");
+            $(this).addClass("active");
+            e.preventDefault();
+        });
+
+    });
+
+    $("[data-widget='remove']").click(function() {
+        //Find the box parent
+        var box = $(this).parents(".box").first();
+        box.slideUp();
+    });
+
+    /*
+     * Make sure that the sidebar is streched full height
+     * ---------------------------------------------
+     * We are gonna assign a min-height value every time the
+     * wrapper gets resized and upon page load. We will use
+     * Ben Alman's method for detecting the resize event.
+     **/
+    //alert($(window).height());
+    function _fix() {
+        //Get window height and the wrapper height
+        var height = $(window).height() - $("body > .header").height();
+        $(".wrapper").css("min-height", height + "px");
+        var content = $(".wrapper").height();
+        //If the wrapper height is greater than the window
+        if (content > height)
+            //then set sidebar height to the wrapper
+            $(".left-side, html, body").css("min-height", content + "px");
+        else {
+            //Otherwise, set the sidebar to the height of the window
+            $(".left-side, html, body").css("min-height", height + "px");
+        }
+    }
+    //Fire upon load
+    _fix();
+    //Fire when wrapper is resized
+    $(".wrapper").resize(function() {
+        _fix();
+    });
+});
