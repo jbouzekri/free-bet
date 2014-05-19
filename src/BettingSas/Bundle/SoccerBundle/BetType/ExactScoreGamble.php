91727084918694d5d925b9a4dc40c6de8d57e2b8
jbouzekri <jonathan.bouzekri@gmail.com>
2014-05-19 22:34:43 +0200

    integration detail bet page

--- src/BettingSas/Bundle/SoccerBundle/BetType/ExactScoreGamble.php
+++ src/BettingSas/Bundle/SoccerBundle/BetType/ExactScoreGamble.php
@@ -13,49 +13,20 @@ use BettingSas\Bundle\GambleBundle\Document\Bet;
 class ExactScoreGamble implements BetTypeInterface
 {
     /**
-     * Choices available in gamble
-     *
-     * @var array
-     */
-    protected $choices = array(
-        "1-0",
-        "2-0",
-        "2-1",
-        "3-0",
-        "3-1",
-        "3-2",
-        "4-0",
-        "4-1",
-        "4-2",
-        "4-3",
-
-        "0-0",
-        "1-1",
-        "2-2",
-        "3-3",
-        "4-4",
-        "5-5",
-
-        "0-1",
-        "0-2",
-        "1-2",
-        "0-3",
-        "1-3",
-        "2-3",
-        "0-4",
-        "1-4",
-        "2-4",
-        "3-4",
-    );
-
-    /**
      * Get choices available in gamble
      *
      * @return array
      */
     public function getChoices()
     {
-        return $this->choices;
+        $choices = array();
+        $choicesNumber = max(count($this->leftChoices)-1, count($this->nullChoices)-1, count($this->rightChoices)-1);
+        for ($i = 0;$i < $choicesNumber;$i++) {
+            $choices[] = $this->leftChoices[$i];
+            $choices[] = (isset($this->nullChoices[$i])) ? $this->nullChoices[$i] : null;
+            $choices[] = $this->rightChoices[$i];
+        }
+        return $choices;
     }
 
     /**
@@ -112,4 +83,103 @@ class ExactScoreGamble implements BetTypeInterface
     {
         return 15;
     }
+
+    /**
+     * Left team winner choices
+     *
+     * @var array
+     */
+    protected $leftChoices = array(
+        "1-0",
+        "2-0",
+        "2-1",
+        "3-0",
+        "3-1",
+        "3-2",
+        "4-0",
+        "4-1",
+        "4-2",
+        "4-3",
+        "5-1",
+        "5-2",
+        "5-3",
+        "5-4",
+        "6-0",
+        "6-1",
+        "6-2",
+        "6-3",
+        "6-4",
+        "6-5",
+        "7-0",
+        "7-1",
+        "7-2",
+        "7-3",
+        "7-4",
+        "8-0",
+        "8-1",
+        "8-2",
+        "8-3",
+        "9-0",
+        "9-1",
+        "9-2",
+        "10-0",
+        "10-1",
+    );
+
+    /**
+     * Null choices
+     *
+     * @var array
+     */
+    protected $nullChoices = array(
+        "0-0",
+        "1-1",
+        "2-2",
+        "3-3",
+        "4-4",
+        "5-5",
+    );
+
+    /**
+     * Right team winner choices
+     *
+     * @var array
+     */
+    protected $rightChoices = array(
+        "0-1",
+        "0-2",
+        "1-2",
+        "0-3",
+        "1-3",
+        "2-3",
+        "0-4",
+        "1-4",
+        "2-4",
+        "3-4",
+        "0-5",
+        "1-5",
+        "2-5",
+        "3-5",
+        "4-5",
+        "0-6",
+        "1-6",
+        "2-6",
+        "3-6",
+        "4-6",
+        "5-6",
+        "0-7",
+        "1-7",
+        "2-7",
+        "3-7",
+        "4-7",
+        "0-8",
+        "1-8",
+        "2-8",
+        "3-8",
+        "0-9",
+        "1-9",
+        "2-9",
+        "0-10",
+        "1-10",
+    );
 }
--- src/BettingSas/Bundle/UIBundle/Resources/public/js/events.js
+++ src/BettingSas/Bundle/UIBundle/Resources/public/js/events.js
@@ -2,13 +2,24 @@ if (typeof jQuery === "undefined") { throw new Error("Event requires jQuery") }
 
 +function ($) {
 
-    $('#next-event-list table tr th').click(function() {
+    $('#next-event-list table tr th, .bet-header').click(function() {
         var $this = $(this);
         var $glyph = $(this).find('span.glyphicon').first();
         $glyph.toggleClass('glyphicon-chevron-down');
         $glyph.toggleClass('glyphicon-chevron-right');
-        $this.parents('table').find('tbody').first().toggle();
+        if ($this.hasClass("bet-header")) {
+            var $body = $this.next();
+        } else {
+            var $body = $this.parents('table').find('tbody').first();
+        }
+        $body.toggle()
         return false;
     });
 
+    $('.toggle').click(function() {
+        var $this = $(this);
+        var toggleClass = $this.data('toggle');
+        $(document).find('.'+toggleClass).toggle();
+        $('.wrapper').trigger('resize');
+    });
 }(jQuery);
