ec15ea6039994a65c1de0e7c399342902bc6acd4
jbouzekri <jonathan.bouzekri@gmail.com>
2014-05-29 19:25:43 +0200

    add global, 404 and 403 error pages

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -55,6 +55,7 @@ class AppKernel extends Kernel
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
             $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
+            $bundles[] = new Webfactory\Bundle\ExceptionsBundle\WebfactoryExceptionsBundle();
         }
 
         return $bundles;
