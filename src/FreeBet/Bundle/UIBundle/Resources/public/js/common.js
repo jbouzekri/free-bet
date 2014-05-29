437b263b8259d050be6825013469574e44259aae
jbouzekri <jonathan.bouzekri@gmail.com>
2014-05-29 20:58:18 +0200

    css mobile optimization

--- src/FreeBet/Bundle/UIBundle/Resources/public/js/common.js
+++ src/FreeBet/Bundle/UIBundle/Resources/public/js/common.js
@@ -52,4 +52,21 @@ $(function() {
     $(".wrapper").resize(function() {
         _fix();
     });
+
+    //Enable sidebar toggle
+    $("[data-toggle='offcanvas']").click(function(e) {
+        e.preventDefault();
+
+        //If window is small enough, enable sidebar push menu
+        if ($(window).width() <= 992) {
+            $('.row-offcanvas').toggleClass('active');
+            $('.left-side').removeClass("collapse-left");
+            $(".right-side").removeClass("strech");
+            $('.row-offcanvas').toggleClass("relative");
+        } else {
+            //Else, enable content streching
+            $('.left-side').toggleClass("collapse-left");
+            $(".right-side").toggleClass("strech");
+        }
+    });
 });
